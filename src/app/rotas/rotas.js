28c95f3fde4414512cb50fe26b259a91dc0e001d
alvaro alves <alvaroalves87@gmail.com>
2020-08-02 18:40:14 -0300

    adding marko module and refining routes

--- src/app/rotas/rotas.js
+++ src/app/rotas/rotas.js
@@ -1,9 +1,21 @@
 module.exports = (app) => {
     app.get('/', function (req, resp){
-        resp.end('<html><head><meta charset="utf-8"></head><body><h1>Hello World!</h1></body></html>')
+        resp.send('<html><head><meta charset="utf-8"></head><body><h1>Hello World!!</h1></body></html>')
     })
     
     app.get('/livros', function (req, resp){
-        resp.end('<html><head><meta charset="utf-8"></head><body><h1>Listagem de livros!</h1></body></html>')
+        resp.marko(
+            require('../views/livros/lista/lista.marko'),{
+                livros: [
+                    {
+                        id: 1,
+                        titulo: "Fundamentos do node"
+                    },{
+                        id: 2,
+                        titulo: "Fundamentos de aprendizagem dinâmica"
+                    }
+                ]
+            }
+        )
     })
 }
