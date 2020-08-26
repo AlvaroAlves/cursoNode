69ba86a58e3b78b2772e39124ce4b97c590d7ee4
alvaro alves <alvaroalves87@gmail.com>
2020-08-26 18:57:50 -0300

    enable bodypaser to receive complex objects in json

--- src/config/custom-express.js
+++ src/config/custom-express.js
@@ -5,6 +5,9 @@ const express = require('express')
 
 const app = express();
 const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({
+    extended: true
+}))
 
 const rotas = require('../app/rotas/rotas')
 
