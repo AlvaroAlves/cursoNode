66606e43e012a614dda7d469f2746aa0254b47d6
alvaro alves <alvaroalves87@gmail.com>
2020-08-29 14:54:26 -0300

    first validation with express validator

--- src/app/rotas/rotas.js
+++ src/app/rotas/rotas.js
@@ -1,14 +1,29 @@
 const LivroDao = require('../infra/livro-dao')
 const db = require('../../config/database')
 const listaMarko = require('../views/livros/lista/lista.marko')
+const { check, validationResult } = require('express-validator/check');
 
 module.exports = (app) => {
     app.get('/', function (req, resp){
         resp.send('<html><head><meta charset="utf-8"></head><body><h1>Hello World!!</h1></body></html>')
     })
     
-    app.get('/livros', function (req, resp){
+    app.get('/livros',[
+            check('titulo').isLength({min : 5}),
+            check('preco').isCurrency()
+        ],
+        (req, resp) => {
         const livroDao = new LivroDao(db)
+
+        const err = validationResult(req)
+
+        if(!err.isEmpty()){
+            return resp.marko(
+                require('../views/livros/form/form.marko'),
+                {livro : {}}
+            )
+        }
+
         livroDao.lista()
             .then(livros => resp.marko(
                 require('../views/livros/lista/lista.marko'),{
