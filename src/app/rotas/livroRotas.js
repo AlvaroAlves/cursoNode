bdeba19fa73289fa2c6c40c72a5449ec6b37d36e
alvaro alves <alvaroalves87@gmail.com>
2020-09-03 16:34:37 -0300

    concluded authentication

--- src/app/rotas/livroRotas.js
+++ src/app/rotas/livroRotas.js
@@ -1,10 +1,19 @@
 const Livro = require('../models/livro')
 
 const LivroController  = require('../controllers/livroController')
+const BaseController = require('../Controllers/baseController')
 const livroController = new LivroController()
 
 module.exports = (app) => {
     const rotasLivro = LivroController.rotas()
+
+    app.use(rotasLivro.autenticadas, (req, resp, next) =>{
+        if(req.isAuthenticated()){
+            next()
+        }else{
+            resp.redirect(BaseController.rotas().login)
+        }
+    })
 
     app.get(rotasLivro.lista, livroController.lista())
 
